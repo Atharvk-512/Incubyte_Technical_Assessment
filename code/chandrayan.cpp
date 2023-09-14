c7f8feee71a59cf064ea1a15132478551df7cb42
Atharvk-512 <atharvk.2019@gmail.com>
2023-09-15 00:29:30 +0530

    added suitable comments

--- code/chandrayan.cpp
+++ code/chandrayan.cpp
@@ -1,12 +1,14 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>    //header files
 using namespace std;
 
+//class Chandrayan declared
 class Chandrayan {
 private:
-    int x, y, z;
-    char direction;
+    int x, y, z;  //x,y,x coordinates 
+    char direction; // direction
 
 public:
+    // parameterized constructor
     Chandrayan(int x_co, int y_co, int z_co, char dir){
         x = x_co;
         y = y_co;
@@ -14,6 +16,7 @@ public:
         direction = dir;
     }
 
+    //member functions
     vector<char> get_input();
     void move_forward();
     void move_backward();
@@ -25,6 +28,7 @@ public:
     void display_status();
 };
 
+// function for taking input from user
 vector<char> Chandrayan :: get_input(){
     vector<char> input;
     char inputchar;
@@ -40,6 +44,7 @@ vector<char> Chandrayan :: get_input(){
     return input;
 }
 
+// function for forward movement
 void Chandrayan::move_forward(){
     if (direction == 'N') y++;
     else if (direction == 'S') y--;
@@ -49,6 +54,7 @@ void Chandrayan::move_forward(){
     else if (direction == 'D') z--;
 }
 
+// function for backward movement
 void Chandrayan::move_backward(){
     if (direction == 'N') y--;
     else if (direction == 'S') y++;
@@ -58,6 +64,7 @@ void Chandrayan::move_backward(){
     else if (direction == 'D') z++;
 }
 
+// function for turning left
 void Chandrayan::turn_left(){
     if (direction == 'N') direction = 'W';
     else if (direction == 'S') direction = 'E';
@@ -65,6 +72,7 @@ void Chandrayan::turn_left(){
     else if (direction == 'W') direction = 'S';
 }
 
+// function for turning right
 void Chandrayan::turn_right(){
     if (direction == 'N') direction = 'E';
     else if (direction == 'S') direction = 'W';
@@ -72,14 +80,18 @@ void Chandrayan::turn_right(){
     else if (direction == 'W') direction = 'N';
 }
 
+// function for turning up
 void Chandrayan::turn_up(){
     if (direction != 'U') direction = 'U';
 }
 
+// function for turning down
 void Chandrayan::turn_down(){
     if (direction != 'D') direction = 'D';
 }
 
+
+//function to execute all the commands sequentially by calling appropriate funtion
 void Chandrayan::execute(vector<char> &commands){
     for(char cmd : commands){
         if (cmd == 'f') move_forward();
@@ -91,6 +103,7 @@ void Chandrayan::execute(vector<char> &commands){
     }
 }
 
+// function to display final position and direction of chandrayan
 void Chandrayan::display_status(){
     cout<<"Final position of Chandrayan3 : ("<<x<<", "<<y<<", "<<z<<")\n";
     cout<<"Final direction of Chandrayan3 : "<<direction<<"\n";
@@ -98,9 +111,12 @@ void Chandrayan::display_status(){
 
 
 int main() {
-    Chandrayan spacecraft(0, 0, 0, 'N');
-    vector<char> commands = spacecraft.get_input();
-    spacecraft.execute(commands);
-    spacecraft.display_status();
+    Chandrayan spacecraft(0, 0, 0, 'N');    // object spacecraft of class Chandryan created
+    vector<char> commands = spacecraft.get_input();     // get_input() called using object spacecraft
+    spacecraft.execute(commands);   // execute() called using object spacecraft
+    spacecraft.display_status();    // display_status() called using object spacecraft
     return 0;
 }
+
+
+// program terminated
